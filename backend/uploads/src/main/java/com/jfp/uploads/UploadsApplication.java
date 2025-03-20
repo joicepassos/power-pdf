e4cecd149baf104b731543e7541c668944a868ba
joicepassos <joicepassos72@gmail.com>
2025-03-19 22:17:55 -0300

    feat: add initial config with uploads api

---
+++ backend/uploads/src/main/java/com/jfp/uploads/UploadsApplication.java
@@ -0,0 +1,12 @@
+package com.jfp.uploads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UploadsApplication.class, args);
+  }
+}
---
+++ backend/uploads/src/test/java/com/jfp/uploads/UploadsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jfp.uploads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
